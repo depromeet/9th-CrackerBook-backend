f71b8b8ddde7598ca30a81533f5e5a9135fa383f
Junhong-Kim <jhk1005jhk@gmail.com>
2021-04-13 23:14:24 +0900

    init

---
+++ src/main/java/com/depromeet/crackerbook/CrackerbookApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.crackerbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackerbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrackerbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/depromeet/crackerbook/CrackerbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.crackerbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackerbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
