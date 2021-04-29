32dac3fe1c11ac68fc98ea0744f6b658ef5c86ed
Connor Kim <k.connor614@gmail.com>
2021-04-30 02:06:26 +0900

    책 도메인 파일 추가

--- crackerbook-core/src/main/java/com/depromeet/crackerbook/domain/book/Book.java
+++ crackerbook-core/src/main/java/com/depromeet/crackerbook/domain/book/Book.java
@@ -1,4 +1,36 @@
 package com.depromeet.crackerbook.domain.book;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private Long id;
+
+    private String name;
+    private String contents;
+    private String imageUrlSmall;
+    private String imageUrlBig;
+    private String authors;
+    private Integer price;
+    private Integer salePrice;
+    private String publisher;
+    private LocalDateTime published_at;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
